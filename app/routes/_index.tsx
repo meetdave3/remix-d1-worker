7477c6734b2a1e4bb2763f11ef5dfdfc79f8b072
Meet Dave <meet.dave@outlook.com>
2023-11-26 23:49:08 +0100

    feat: fetch users list from db

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,61 @@
-// import { useLoaderData } from "@remix-run/react";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
-import type { MetaFunction } from "@remix-run/cloudflare";
+interface Env {
+  DB: D1Database;
+}
 
-// interface Env {
-//   // todo
-// }
+interface User {
+  CustomerId: number,
+  CompanyName: string,
+  ContactName: string,
+}
 
-// interface LoaderData {
-//   // todo
-// }
+interface LoaderData {
+  users: User[];
+}
 
-// export async function loader(props: LoaderFunctionArgs) {
-//   const env = props.context.env as Env;
-// }
+export async function loader(props: LoaderFunctionArgs) {
+  const env = props.context.env as Env;
+
+  const { results } = await env.DB.prepare(
+    "SELECT * FROM Customers"
+  ).bind().all();
+
+  return json({
+    users: results,
+  })
+}
 
 export default function Index() {
-  // const props = useLoaderData<LoaderData>();
+  const { users } = useLoaderData<LoaderData>();
 
   return (
     <div className="max-w-5xl m-auto p-10">
       <h1 className="text-3xl font-bold w-4/5">
         Database access on the edge with Remix run, Cloudflare workers & D1
       </h1>
+
+      <div className="mt-10">
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 text-left">Customer ID</th>
+              <th className="px-4 py-2 text-left">Company Name</th>
+              <th className="px-4 py-2 text-left">Contact Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.CustomerId}>
+                <td className="border px-4 py-2">{user.CustomerId}</td>
+                <td className="border px-4 py-2">{user.CompanyName}</td>
+                <td className="border px-4 py-2">{user.ContactName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
